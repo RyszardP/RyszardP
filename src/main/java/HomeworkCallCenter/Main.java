8851745b0f1b2d91bc7f345ca6c68d9bb6fed6a9
RyszardP <44649892+RyszardP@users.noreply.github.com>
2019-01-07 19:39:38 +0300

    07/01/2019 update

--- src/main/java/HomeworkCallCenter/Main.java
+++ src/main/java/HomeworkCallCenter/Main.java
@@ -5,7 +5,7 @@ public class Main{
 
     public static void main(String[] args) throws InterruptedException {
         for (int i = 1; i <= 10; i++) {
-            new Thread(new SemaphoreRun("Client#" + i, i)).start();
+            new Thread(new SemaphoreRun(NameGenerator.generateName() + i, i)).start();
             Thread.sleep(400);
         }
     }
